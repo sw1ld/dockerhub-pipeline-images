e50f913fc5d6df312574c3ad0e2da79644f0e8c3
Jan-Otto Kröpke <mail@jkroepke.de>
2018-07-01 19:04:27 +0200

    Add docker-compose.yml

--- gen_compose.sh
+++ gen_compose.sh
@@ -4,10 +4,9 @@ echo "---" > docker-compose.yml
 echo "version: '3'" >> docker-compose.yml
 echo "services:" >> docker-compose.yml
 
-for file in */Dockerfile */*/Dockerfile; do
-  echo "  ${file//[\/-]/_}:" >> docker-compose.yml
+for FILE in */Dockerfile */*/Dockerfile; do
+  BASENAME=${FILE%/Dockerfile}
+  echo "  ${BASENAME//[\/-]/_}:" >> docker-compose.yml
   echo "    build:" >> docker-compose.yml
-  echo "      context: ${file%Dockerfile}" >> docker-compose.yml
-  echo "    deploy:" >> docker-compose.yml
-  echo "      replicas: 0" >> docker-compose.yml
+  echo "      context: ${BASENAME}" >> docker-compose.yml
 done

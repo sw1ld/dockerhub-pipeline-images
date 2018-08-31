5f7fcb5e6bd2815fceb52283b608d32e8bd2c685
Jan-Otto Kröpke <mail@jkroepke.de>
2018-08-31 23:57:27 +0200

    Remove node 6

--- build/upstream/tests.sh
+++ build/upstream/tests.sh
@@ -8,5 +8,6 @@ docker run --rm ${DOCKER_IMAGE}:${TAG} /bin/bash -lc '\
     echo "Docker: " && docker --version && \
     echo "Java: " && java -version && \
     echo "Maven: " && mvn --version && \
+    echo "JMeter: " && jmeter -n --version && \
     echo "Chrome: " && google-chrome-stable --version && \
     echo "Firefox: " && firefox --version'

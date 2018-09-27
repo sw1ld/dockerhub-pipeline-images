1b57cda569fe9e96e3d210c7aa02c80be3e7b92a
Jan-Otto Kröpke <mail@jkroepke.de>
2018-09-27 13:38:26 +0200

    Add helm secrets

--- helm/2.11/tests.sh
+++ helm/2.11/tests.sh
@@ -5,4 +5,5 @@ docker run --rm ${DOCKER_IMAGE}:${TAG} ansible --version
 docker run --rm ${DOCKER_IMAGE}:${TAG} helm version -c
 docker run --rm -e HELM_HOST=localhost ${DOCKER_IMAGE}:${TAG} helm diff -h
 docker run --rm ${DOCKER_IMAGE}:${TAG} helm push -h
+docker run --rm ${DOCKER_IMAGE}:${TAG} helm secrets -h
 docker run --rm ${DOCKER_IMAGE}:${TAG} copy-docker-image --help

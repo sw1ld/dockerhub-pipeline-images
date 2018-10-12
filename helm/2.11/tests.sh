8927d5f0cf5b4eef3a82b6d6eb5f3adf30d689e3
Jan-Otto Kröpke <mail@jkroepke.de>
2018-10-12 17:16:08 +0200

    Add Helm local tiller

--- helm/2.11/tests.sh
+++ helm/2.11/tests.sh
@@ -6,4 +6,6 @@ docker run --rm ${DOCKER_IMAGE}:${TAG} helm version -c
 docker run --rm -e HELM_HOST=localhost ${DOCKER_IMAGE}:${TAG} helm diff -h
 docker run --rm ${DOCKER_IMAGE}:${TAG} helm push -h
 docker run --rm ${DOCKER_IMAGE}:${TAG} helm secrets -h
+docker run --rm ${DOCKER_IMAGE}:${TAG} helm tiller -h
+docker run --rm -eHELM_TILLER_SILENT=false ${DOCKER_IMAGE}:${TAG} helm tiller run helm version
 docker run --rm ${DOCKER_IMAGE}:${TAG} copy-docker-image --help

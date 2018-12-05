eea9d8bc45ace22ba6d0144f42540b146045f45c
Jan-Otto Kröpke <mail@jkroepke.de>
2018-12-05 19:44:24 +0100

    Added Tests for clair

---
+++ ci-clair/tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run --rm ${DOCKER_IMAGE}:${TAG} test -f /clair
+docker run --rm ${DOCKER_IMAGE}:${TAG} test -f /config/config.yaml
+docker run --rm ${DOCKER_IMAGE}:${TAG} clair-scanner -h
+docker run --rm ${DOCKER_IMAGE}:${TAG} docker --version

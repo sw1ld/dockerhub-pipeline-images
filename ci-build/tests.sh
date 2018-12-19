0cf975955ec8c5bb0f1df5848ac0f996168cb437
Jan-Otto Kröpke <mail@jkroepke.de>
2018-12-19 21:50:39 +0100

    Extend tests for ci-build

--- ci-build/tests.sh
+++ ci-build/tests.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-docker run --rm ${DOCKER_IMAGE}:${TAG} /bin/bash -lc '\
-    echo "NVM: " && nvm --version && \
-    echo "NodeJS: " && node --version && \
-    echo "NPM (NodeJS): " && npm --version && \
-    echo "YARN: " && yarn --version && \
-    echo "Docker: " && docker --version && \
-    echo "Java: " && java -version && \
-    echo "Maven: " && mvn --version && \
-    echo "JMeter: " && jmeter -n --version && \
-    echo "Chrome: " && google-chrome-stable --version && \
-    echo "Firefox: " && firefox --version && \
-    echo "Skopeo: " && skopeo -v'
+set -x
+
+docker run --rm ${DOCKER_IMAGE}:${TAG} bash --version
+
+docker run --rm ${DOCKER_IMAGE}:${TAG} docker --version
+docker run --rm ${DOCKER_IMAGE}:${TAG} java -version
+docker run --rm ${DOCKER_IMAGE}:${TAG} mvn --version
+docker run --rm ${DOCKER_IMAGE}:${TAG} jmeter -n --version
+docker run --rm ${DOCKER_IMAGE}:${TAG} google-chrome-stable --version
+docker run --rm ${DOCKER_IMAGE}:${TAG} firefox --version
+docker run --rm ${DOCKER_IMAGE}:${TAG} skopeo -v
+
+docker run --rm ${DOCKER_IMAGE}:${TAG} ip -V
+
+# Default should be java 8
+docker run --rm --entrypoint=/bin/bash ${DOCKER_IMAGE}:${TAG} -c 'java -version 2>&1 | grep -q "build 1\.8"'
+
+docker run --rm --entrypoint=/bin/bash ${DOCKER_IMAGE}:${TAG} -c 'jabba use system@1.8 && java -version 2>&1 | grep -q "build 1\.8"'
+docker run --rm --entrypoint=/bin/bash ${DOCKER_IMAGE}:${TAG} -c 'jabba use system@1.11 && java -version 2>&1 | grep -q "build 11"'
+
+docker run --rm --entrypoint=/bin/bash ${DOCKER_IMAGE}:${TAG} -c 'nvm exec 8 node --version | grep -q "node v8"'
+docker run --rm --entrypoint=/bin/bash ${DOCKER_IMAGE}:${TAG} -c 'nvm exec 10 node --version | grep -q "node v10"'
+
+docker run --rm --entrypoint=/bin/bash ${DOCKER_IMAGE}:${TAG} -c 'nvm exec 8 node --version'
+docker run --rm --entrypoint=/bin/bash ${DOCKER_IMAGE}:${TAG} -c 'nvm exec 10 node --version'
+
+docker run --rm --entrypoint=/bin/bash ${DOCKER_IMAGE}:${TAG} -c 'nvm exec 8 npm --version'
+docker run --rm --entrypoint=/bin/bash ${DOCKER_IMAGE}:${TAG} -c 'nvm exec 10 npm --version'
+
+docker run --rm --entrypoint=/bin/bash ${DOCKER_IMAGE}:${TAG} -c 'nvm exec 8 yarn --version'
+docker run --rm --entrypoint=/bin/bash ${DOCKER_IMAGE}:${TAG} -c 'nvm exec 10 yarn --version'

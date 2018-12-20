ef8a47f942a298c625f044a7e75fc31d0d1253cf
Jan-Otto Kröpke <mail@jkroepke.de>
2018-12-20 14:07:54 +0100

    Use go yq instead pip yq >=2.11

--- ci-helm/2.10/tests.sh
+++ ci-helm/2.10/tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker run --rm ${DOCKER_IMAGE}:${TAG} docker --version
+docker run --rm ${DOCKER_IMAGE}:${TAG} git --version
 docker run --rm ${DOCKER_IMAGE}:${TAG} ansible --version
 docker run --rm ${DOCKER_IMAGE}:${TAG} helm version -c
 docker run --rm ${DOCKER_IMAGE}:${TAG} helm-wrapper version -c
--- ci-helm/2.12/tests.sh
+++ ci-helm/2.12/tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker run --rm ${DOCKER_IMAGE}:${TAG} docker --version
+docker run --rm ${DOCKER_IMAGE}:${TAG} git --version
 docker run --rm ${DOCKER_IMAGE}:${TAG} helm version -c
 docker run --rm ${DOCKER_IMAGE}:${TAG} helm-wrapper version -c
 docker run --rm -e HELM_HOST=localhost ${DOCKER_IMAGE}:${TAG} helm diff -h

593b22c1a653ed1db73b908b69b938bd72e77c28
Jan-Otto Kröpke <mail@jkroepke.de>
2018-12-20 13:35:58 +0100

    no ansible in 2.12

--- ci-helm/2.12/tests.sh
+++ ci-helm/2.12/tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker run --rm ${DOCKER_IMAGE}:${TAG} docker --version
-docker run --rm ${DOCKER_IMAGE}:${TAG} ansible --version
 docker run --rm ${DOCKER_IMAGE}:${TAG} helm version -c
 docker run --rm ${DOCKER_IMAGE}:${TAG} helm-wrapper version -c
 docker run --rm -e HELM_HOST=localhost ${DOCKER_IMAGE}:${TAG} helm diff -h

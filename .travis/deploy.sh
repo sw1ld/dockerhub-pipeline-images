3c271135b21f79619956953a5b5f372385f1fa1f
Jan-Otto Kröpke <mail@jkroepke.de>
2019-01-03 23:48:08 +0100

    Multiple retries at docker push

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,16 +4,16 @@ set -xeuo pipefail
 
 echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USER}" --password-stdin
 echo "push to ${DOCKER_IMAGE}:${TAG}"
-docker push "${DOCKER_IMAGE}:${TAG}"
+travis_retry docker push "${DOCKER_IMAGE}:${TAG}"
 
 if [ -n "${ALIASES+x}" ]; then
   echo "Pushing tag aliases ${ALIASES}"
   docker tag "${DOCKER_IMAGE}:${TAG}" "${DOCKER_IMAGE}:${ALIASES}"
-  docker push "${DOCKER_IMAGE}:${ALIASES}"
+  travis_retry docker push "${DOCKER_IMAGE}:${ALIASES}"
 fi
 
 if [ -n "${SNAPSHOT+x}" ] && [ "$(date +%d)" -eq "1" ]; then
   echo "Pushing snapshot tag $(date +%Y%m)"
   docker tag "${DOCKER_IMAGE}:${TAG}" "${DOCKER_IMAGE}:$(date +%Y%m)"
-  docker push "${DOCKER_IMAGE}:$(date +%Y%m)"
+  travis_retry docker push "${DOCKER_IMAGE}:$(date +%Y%m)"
 fi

80d265eb5fcabdd2db9e07b626b707b5fdb0c1bf
Jan-Otto Kröpke <mail@jkroepke.de>
2019-01-24 19:11:53 +0100

    Add NGINX_DISABLE_ACCESS_LOG for adorsys/nginx:alpine

--- nginx/alpine/README.md
+++ nginx/alpine/README.md
@@ -6,3 +6,9 @@ Paths:
 * `/usr/share/nginx/html`: Webroot
 * `/etc/nginx/conf.d/`: Configs for inside http block
 * `/etc/nginx/conf.default.d/` Configs for inside server block
+
+Environments
+
+| Key | Description | Default |
+|-----|-------------|---------|
+| NGINX_DISABLE_ACCESS_LOG | If `true`, disable access log in console | `unset` |
--- nginx/alpine/root/docker-entrypoint.sh
+++ nginx/alpine/root/docker-entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+set -euo pipefail
+
 # From https://docs.openshift.com/container-platform/3.9/creating_images/guidelines.html#use-uid
 if ! whoami > /dev/null 2>&1; then
   if [ -w /etc/passwd ]; then
     echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
   fi
+fi
+
+if [ "${NGINX_DISABLE_ACCESS_LOG}" == "true" ]; then
+  echo "access_log off;" > /etc/nginx/conf.d/logging.conf
 fi
 
 # envsubst

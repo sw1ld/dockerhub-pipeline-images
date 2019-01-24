6c50e2e38b441a73e6254b937ae0afc1a64b165a
Jan-Otto Kröpke <mail@jkroepke.de>
2019-01-24 19:21:52 +0100

    nginx: Fixed error if env is unset

--- nginx/alpine/tests.sh
+++ nginx/alpine/tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker run --rm "${DOCKER_IMAGE}:${TAG}" nginx -t
+docker run -eNGINX_DISABLE_ACCESS_LOG=true --rm "${DOCKER_IMAGE}:${TAG}" nginx -t
 
 # Test as Openshift UID
 docker run --rm -u 1000090000:0 "${DOCKER_IMAGE}:${TAG}" whoami

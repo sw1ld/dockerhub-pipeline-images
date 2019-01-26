1ee94c5380f16eb4d80edd84bbc2f75398a651b7
Jan-Otto Kröpke <mail@jkroepke.de>
2019-01-26 23:21:04 +0100

    Do not run builds on travis twice

--- gen_compose.sh
+++ gen_compose.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
+{
+  echo "---"
+  echo "version: '3'"
+  echo "services:"
 
-echo "---" > docker-compose.yml
-echo "version: '3'" >> docker-compose.yml
-echo "services:" >> docker-compose.yml
-
-for FILE in */Dockerfile */*/Dockerfile */*/*/Dockerfile; do
-  BASENAME=${FILE%/Dockerfile}
-  if [[ "$FILE" == */*/*/Dockerfile ]]; then
-    # $1/$2/$3 -> adorsys/$1:$3-$2
-    IMAGE="adorsys/$(echo "${BASENAME}" | cut -d/ -f1):$(echo "${BASENAME}" | cut -d/ -f3)-$(echo "${BASENAME}" | cut -d/ -f2)"
-  else
-    # $1/$2 -> adorsys/$1:$2
-    IMAGE="adorsys/${BASENAME/\//:}"
-  fi
-  echo "  ${BASENAME//[\/-]/_}:" >> docker-compose.yml
-  echo "    image: ${IMAGE}" >> docker-compose.yml
-  echo "    build:" >> docker-compose.yml
-  echo "      context: ${BASENAME}" >> docker-compose.yml
-done
+  for FILE in */Dockerfile */*/Dockerfile */*/*/Dockerfile; do
+    BASENAME=${FILE%/Dockerfile}
+    if [[ "$FILE" == */*/*/Dockerfile ]]; then
+      # $1/$2/$3 -> adorsys/$1:$3-$2
+      IMAGE="adorsys/$(echo "${BASENAME}" | cut -d/ -f1):$(echo "${BASENAME}" | cut -d/ -f3)-$(echo "${BASENAME}" | cut -d/ -f2)"
+    else
+      # $1/$2 -> adorsys/$1:$2
+      IMAGE="adorsys/${BASENAME/\//:}"
+    fi
+    echo "  ${BASENAME//[\/-]/_}:"
+    echo "    image: ${IMAGE}"
+    echo "    build:"
+    echo "      context: ${BASENAME}"
+  done
+} > docker-compose.yml

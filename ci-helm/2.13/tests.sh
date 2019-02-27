c10f88c1dd332c48eb8986287be58cece3b32d85
Jan-Otto Kröpke <mail@jkroepke.de>
2019-02-27 20:17:35 +0100

    Add helm 2.13

---
+++ ci-helm/2.13/tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker run --rm "${DOCKER_IMAGE}:${TAG}" docker --version
+docker run --rm "${DOCKER_IMAGE}:${TAG}" git --version
+docker run --rm "${DOCKER_IMAGE}:${TAG}" helm version -c
+docker run --rm "${DOCKER_IMAGE}:${TAG}" helm-wrapper version -c
+docker run --rm -e HELM_HOST=localhost "${DOCKER_IMAGE}:${TAG}" helm diff -h
+docker run --rm "${DOCKER_IMAGE}:${TAG}" helm push -h
+docker run --rm "${DOCKER_IMAGE}:${TAG}" helm secrets -h
+docker run --rm "${DOCKER_IMAGE}:${TAG}" helm tiller -h
+docker run --rm -eHELM_TILLER_SILENT=false "${DOCKER_IMAGE}:${TAG}" helm tiller run helm version
+docker run --rm "${DOCKER_IMAGE}:${TAG}" skopeo -v
+docker run --rm "${DOCKER_IMAGE}:${TAG}" gpg --list-keys
+docker run --rm "${DOCKER_IMAGE}:${TAG}" sops -v
+docker run --rm "${DOCKER_IMAGE}:${TAG}" gpg-preset-passphrase -h
+docker run --rm "${DOCKER_IMAGE}:${TAG}" yq --version
+docker run --rm "${DOCKER_IMAGE}:${TAG}" jq --version
+docker run --rm "${DOCKER_IMAGE}:${TAG}" unzip -v
+docker run --rm "${DOCKER_IMAGE}:${TAG}" skopeo copy docker://docker.io/library/alpine dir:///tmp/alpine.tar
+docker run --rm "${DOCKER_IMAGE}:${TAG}" envsubst --version
+docker run --rm "${DOCKER_IMAGE}:${TAG}" rush -V

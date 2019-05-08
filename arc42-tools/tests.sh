ef70a3033d15bbc82ca901345028cd136be0c901
Denys Golubiev <dgo@adorsys.de>
2019-05-08 13:07:03 +0200

    Fix plantuml version command

--- arc42-tools/tests.sh
+++ arc42-tools/tests.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-docker run --rm "${DOCKER_IMAGE}:${TAG}" plantuml -v
+docker run --rm "${DOCKER_IMAGE}:${TAG}" plantuml -version
 docker run --rm "${DOCKER_IMAGE}:${TAG}" asciidoctor -v

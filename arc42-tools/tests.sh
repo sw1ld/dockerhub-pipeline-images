2205e88f47dba66f07b21c1c56e1790a46346c19
Denys Golubiev <dgo@adorsys.de>
2019-07-08 10:58:57 +0200

    Assure that make and rsync are installed

--- arc42-tools/tests.sh
+++ arc42-tools/tests.sh
@@ -4,3 +4,5 @@ set -euo pipefail
 
 docker run --rm "${DOCKER_IMAGE}:${TAG}" plantuml -version
 docker run --rm "${DOCKER_IMAGE}:${TAG}" asciidoctor -v
+docker run --rm "${DOCKER_IMAGE}:${TAG}" make -v
+docker run --rm "${DOCKER_IMAGE}:${TAG}" rsync --version

7a1f58260823fe4b6a93592f7bec59f9289afe74
Jan-Otto Kröpke <mail@jkroepke.de>
2019-10-28 19:07:59 +0100

    Fixed tests on GH infra

--- node/centos/10/tests.sh
+++ node/centos/10/tests.sh
@@ -12,5 +12,5 @@ docker run --rm -u 1000090000:0 "${DOCKER_IMAGE}:${TAG}" whoami
 docker run --rm -v "$(git rev-parse --show-toplevel)/.tests/test-docker-entrypoint.d.sh":/docker-entrypoint.d/test.sh "${DOCKER_IMAGE}:${TAG}" node --version | grep "TEST-ENTRYPOINT-HOOK-WORKS!"
 
 # Test Memory autconfig
-docker run -m 128M --rm -ti "${DOCKER_IMAGE}:${TAG}" env | grep NODE_OPTIONS=--max_old_space_size=107
-docker run -e NODE_MEMORY_LIMIT_PERCENT=50 -m 128MB --rm -ti "${DOCKER_IMAGE}:${TAG}" env | grep NODE_OPTIONS=--max_old_space_size=67
+docker run -m 128M --rm "${DOCKER_IMAGE}:${TAG}" env | grep NODE_OPTIONS=--max_old_space_size=107
+docker run -e NODE_MEMORY_LIMIT_PERCENT=50 -m 128MB --rm "${DOCKER_IMAGE}:${TAG}" env | grep NODE_OPTIONS=--max_old_space_size=67

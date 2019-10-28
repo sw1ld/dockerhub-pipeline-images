ef94b2fca08b238a759fa6b0a7950e7185f700b1
Jan-Otto Kröpke <mail@jkroepke.de>
2019-10-28 19:03:27 +0100

    Added more dgoss examples

--- java/ubi/8/tests.sh
+++ java/ubi/8/tests.sh
@@ -2,16 +2,10 @@
 
 set -euo pipefail
 
-docker run --rm "${DOCKER_IMAGE}:${TAG}" java -version
-docker run --rm "${DOCKER_IMAGE}:${TAG}" openssl version
-docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'date | grep -E "CES?T"'
-docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'ldd /usr/lib/libtcnative-1.so | grep -vz "=> not found"'
-
-docker run --rm -eSPRING_MAIN_BANNER-MODE=off -v "$(git rev-parse --show-toplevel)/.tests/java/example-app/target/dockerhub-pipeline-images-test-jar.jar":/opt/app-root/src/app.jar "${DOCKER_IMAGE}:${TAG}"
-docker run --rm -eSPRING_MAIN_BANNER-MODE=off -v "$(git rev-parse --show-toplevel)/.tests/java/example-app/target/dockerhub-pipeline-images-test-jar.jar":/opt/app-root/src/app.jar "${DOCKER_IMAGE}:${TAG}" | grep "Loaded APR based Apache Tomcat Native library"
-
-# Test as Openshift UID
-docker run --rm -u 1000090000:0 "${DOCKER_IMAGE}:${TAG}" whoami
-
-# Test /docker-entrypoint.d/*.sh
-docker run --rm -v "$(git rev-parse --show-toplevel)/.tests/test-docker-entrypoint.d.sh":/docker-entrypoint.d/test.sh "${DOCKER_IMAGE}:${TAG}" java -version | grep "TEST-ENTRYPOINT-HOOK-WORKS!"
+dgoss run \
+  -u 1000090000:0 \
+  -e SPRING_MAIN_BANNER-MODE=off \
+  -v "$(git rev-parse --show-toplevel)/.tests/test-docker-entrypoint.d.sh":/docker-entrypoint.d/test.sh \
+  -v "$(git rev-parse --show-toplevel)/.tests/java/example-app/target/dockerhub-pipeline-images-test-jar.jar":/opt/app-root/src/app.jar \
+  "${DOCKER_IMAGE}:${TAG}" \
+  sleep inf

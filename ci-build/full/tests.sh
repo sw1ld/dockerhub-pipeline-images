5716d65a3c4551503aea038acf85e51ba9ff9917
Jan-Otto Kröpke <mail@jkroepke.de>
2019-11-07 21:06:14 +0100

    Switch to UBI for ci-build (#65)

--- ci-build/full/tests.sh
+++ ci-build/full/tests.sh
@@ -26,16 +26,16 @@ docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'jabba use system@1.8 && java -
 docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'jabba use system@1.11 && java -version 2>&1 | grep -q "build 11"'
 
 docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm exec 10 node --version | grep -q "node v10"'
-docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm install 12 && nvm exec 12 node --version | grep -q "node v12"'
+docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm exec 12 node --version | grep -q "node v12"'
 
 docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm exec 10 node --version'
-docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm install 12 && nvm exec 12 node --version'
+docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm exec 12 node --version'
 
 docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm exec 10 npm --version'
-docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm install 12 && nvm exec 12 npm --version'
+docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm exec 12 npm --version'
 
 docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm exec 10 yarn --version'
-docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm install 12 && nvm exec 12 yarn --version'
+docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'nvm exec 12 yarn --version'
 
 chmod -R 777 "$(git rev-parse --show-toplevel)/.tests/"
 
@@ -45,6 +45,8 @@ docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'yarn add iconv'
 
 # Test Maven
 docker run --rm -v "$(git rev-parse --show-toplevel)/.tests/java/example-app/":/opt/app-root/src:cached "${DOCKER_IMAGE}:${TAG}" mvn -q --batch-mode clean package
-
+docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'ldd /usr/lib/libtcnative-1.so | grep -vz "=> not found"'
 # Test Headless chrome via angular
-docker run --rm -v "$(git rev-parse --show-toplevel)/.tests/js/test-app/":/opt/app-root/src/:cached "${DOCKER_IMAGE}:${TAG}" bash -c 'npm install && npx ng test --watch=false --code-coverage --browsers ChromeHeadlessNoSandbox'
+docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'npm install puppeteer && ldd ./node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/chrome | grep -vz "=> not found"'
+docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'npm install puppeteer && ./node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/chrome --version'
+docker run --rm -eCI=1 -v "$(git rev-parse --show-toplevel)/.tests/js/example-app/":/opt/app-root/src/:cached "${DOCKER_IMAGE}:${TAG}" bash -c 'npm install && npx ng test --watch=false --code-coverage --browsers ChromeHeadlessNoSandbox'
